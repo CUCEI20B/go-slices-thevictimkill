b8aed237e85aad61bec15c43dda982b4a51bf1d9
thevictimkill <pedrof.ortega@alumnos.udg.mx>
2020-10-14 23:19:23 +0000

    Version final de la actividad

--- main.go
+++ main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+	var size int
+  fmt.Scan(&size)
+
+  var slice []int
+  var aux int
+
+  for  i:=0; i<size; i++ {
+    fmt.Scan(&aux)
+    slice = append(slice, aux)
+    //fmt.Println(slice)
+  }
+  suma := 0
+  for _, v:= range slice {
+    suma += v
+  }
+  fmt.Println(suma)
 }
